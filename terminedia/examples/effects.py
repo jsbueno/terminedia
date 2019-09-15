0461fe9db1bcc3fd23b0a650b8c58116e51e0225
Joao S O Bueno <gwidion@gmail.com>
2019-09-15 02:28:45 -0300

    Terminal text effects example

---
+++ terminedia/examples/effects.py
@@ -0,0 +1,22 @@
+import click
+
+import terminedia as TM
+
+@click.command()
+@click.argument("phrases", required=False, nargs=-1)
+def main(phrases):
+    if not phrases:
+        phrases = ["Hello World!"] * len(TM.Effects)
+
+    with TM.Screen() as sc:
+        for line, (phrase, effect) in enumerate(zip(phrases, TM.Effects)):
+            sc.context.effects = TM.Effects.none
+
+            sc.print_at((0, line), f"{effect.name}: ")
+            sc.context.effects = effect
+            sc.print(phrase)
+        TM.pause()
+
+
+if __name__ == "__main__":
+    main()
