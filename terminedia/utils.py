d286af05befc90767a8adacce4d46d8ed314649b
Joao S O Bueno <gwidion@gmail.com>
2020-02-14 00:20:18 -0300

    Combining signature decorator added

--- terminedia/utils.py
+++ terminedia/utils.py
@@ -622,6 +622,86 @@ def tick_forward():
     context.ticks = get_current_tick() + 1
 
 
+def combine_signatures(func, wrapper=None):
+    """Adds keyword-only parameters from wrapper to signature
+
+    Use this in place of `functools.wraps`
+    It works by creating a dummy function with the attrs of func, but with
+    extra, KEYWORD_ONLY parameters from 'wrapper'.
+    To be used in decorators that add new keyword parameters as
+    the "__wrapped__"
+
+    Usage:
+
+    def decorator(func):
+        @combine_signatures(func)
+        def wrapper(*args, new_parameter=None, **kwargs):
+            ...
+            return func(*args, **kwargs)
+    """
+    # TODO: move this into 'extradeco' independent package
+    from functools import partial, wraps
+    from inspect import signature, _empty as insp_empty, _ParameterKind as ParKind
+    from itertools import groupby
+
+    if wrapper is None:
+        return partial(combine_signatures, func)
+
+    sig_func = signature(func)
+    sig_wrapper = signature(wrapper)
+    pars_func = {group:list(params)  for group, params in groupby(sig_func.parameters.values(), key=lambda p: p.kind)}
+    pars_wrapper = {group:list(params)  for group, params in groupby(sig_wrapper.parameters.values(), key=lambda p: p.kind)}
+
+    def render_annotation(p):
+        return f"{':' + (repr(p.annotation) if not isinstance(p.annotation, type) else repr(p.annotation.__name__)) if p.annotation != insp_empty else ''}"
+
+    def render_params(p):
+        return f"{'=' + repr(p.default) if p.default != insp_empty else ''}"
+
+    def render_by_kind(groups, key):
+        parameters = groups.get(key, [])
+        return [f"{p.name}{render_annotation(p)}{render_params(p)}" for p in parameters]
+
+    pos_only = render_by_kind(pars_func, ParKind.POSITIONAL_ONLY)
+    pos_or_keyword = render_by_kind(pars_func, ParKind.POSITIONAL_OR_KEYWORD)
+    var_positional = [p for p in pars_func.get(ParKind.VAR_POSITIONAL,[])]
+    keyword_only = render_by_kind(pars_func, ParKind.KEYWORD_ONLY)
+    var_keyword = [p for p in pars_func.get(ParKind.VAR_KEYWORD,[])]
+
+    extra_parameters = render_by_kind(pars_wrapper, ParKind.KEYWORD_ONLY)
+
+    def opt(seq, value=None):
+        return ([value] if value else [', '.join(seq)]) if seq else []
+
+    annotations = func.__annotations__.copy()
+    for parameter in pars_wrapper.get(ParKind.KEYWORD_ONLY):
+        annotations[parameter.name] = parameter.annotation
+
+    param_spec = ', '.join([
+        *opt(pos_only),
+        *opt(pos_only, '/'),
+        *opt(pos_or_keyword),
+        *opt(keyword_only or extra_parameters, ('*' if not var_positional else f"*{var_positional[0].name}")),
+        *opt(keyword_only),
+        *opt(extra_parameters),
+        *opt(var_keyword, f"**{var_keyword[0].name}" if var_keyword else "")
+    ])
+    declaration = f"def {func.__name__}({param_spec}): pass"
+
+    f_globals = func.__globals__
+    f_locals = {}
+
+    exec(declaration, f_globals, f_locals)
+
+    result = f_locals[func.__name__]
+    result.__qualname__ = func.__qualname__
+    result.__doc__ = func.__doc__
+    result.__annotations__ = annotations
+
+    return wraps(result)(wrapper)
+
+
+
 def contextkwords(func=None, context_path=None):
     if func is None:
         return partial(contextkwords, context_path=context_path)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,32 @@
+import inspect
+from inspect import signature
+
 import pytest
+
+from terminedia.utils import combine_signatures
+
+def test_combine_signatures_works():
+    context = {}
+    def add_color(func):
+        @combine_signatures(func)
+        def wrapper(*args, color=None, **kwargs):
+            nonlocal context
+            context["color"] = color
+            return func(*args, **kwargs)
+        return wrapper
+    @add_color
+    def line(p1, p2):
+        assert p1 == (0, 0)
+        assert p2 == (10,10)
+
+    sig = signature(line)
+
+    assert 'p1' in  sig.parameters
+    assert 'p2' in sig.parameters
+    assert 'color' in sig.parameters
+    assert sig.parameters['color'].kind == inspect._ParameterKind.KEYWORD_ONLY
+
+    line((0,0), p2=(10,10), color="red")
+    assert context["color"] == "red"
+
+
