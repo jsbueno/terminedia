8adc2f59dd01c33ccb7a5fe9ded9c38e20e9bf3d
Joao S O Bueno <gwidion@gmail.com>
2020-02-15 02:12:59 -0300

    initial Gradient class

--- terminedia/utils.py
+++ terminedia/utils.py
@@ -648,6 +648,44 @@ class SpecialColor(Color):
         return self.component_source
 
 
+class Gradient:
+    def __init__(self, stops):
+        """Define a gradient.
+        Args:
+           stops: list where each component is a 2-tuple - the first item
+           is a 0<= numbr <= 1, the second is a color.
+
+        use __getitem__ (grdient[0.3]) to get the color value at that point.
+        """
+        self.stops = sorted(stops, key=lambda stop: (stop[0], stops.index(stop)))
+
+    def __getitem__(self, position):
+        p_previous = -1
+        c_previous = self.stops[0][1]
+        for p_start, color, *_ in self.stops:
+            if p_previous < position <= p_start:
+                break
+            p_previous = p_start
+            c_previous = color
+        else:
+            return color
+        if p_previous == -1 or p_start == p_previous:
+            return color
+        scale = 1 / (p_start - p_previous)
+        weight_from_previous = 1 - ((position - p_previous) * scale)
+        weight_from_next = 1 - ((p_start - position) * scale)
+
+        c_previous = c_previous.normalized
+        color = color.normalized
+        return Color((
+            min(1.0, c_previous[i] * weight_from_previous + color[i] * weight_from_next) for i in (0,1,2)
+        ))
+
+    def __repr__(self):
+        return f"Gradient({self.stops})"
+
+
+
 class HookList(MutableSequence):
     def __init__(self, initial=None):
         self.data = list()
