3ac79bd2c3c5877c0c5ee9a76638fda0ba2caf74
Joao S O Bueno <gwidion@gmail.com>
2020-02-15 23:53:28 -0300

    Promote 'Character' class to a subclass of str

--- terminedia/unicode.py
+++ terminedia/unicode.py
@@ -8,25 +8,40 @@ from collections import namedtuple
 from functools import lru_cache
 
 
-Character = namedtuple("Character", "code char name category width")
-
 CHAR_BASE = None
+
+
+class Character(str):
+    __slots__ = "code char name category width".split()
+
+    def __new__(cls, value, code, name, category, width):
+        self = super().__new__(cls, value)
+        self.code = code
+        self.name = name
+        self.category = category
+        self.width = width
+
+        return self
+
+    def __repr__(self):
+            return f"Character(code=0x{self.code:04X}, value='{self}', name='{self.name}', category='{self.category}', width='{self.width}')"
+
 
 def _init_chars():
     global CHAR_BASE
     if CHAR_BASE:
         return
     CHAR_BASE = {}
-    for i in range(0, 0x10ffff):
-        char = chr(i)
-        attrs = "name category east_asian_width"
+    for code in range(0, 0x10ffff):
+        char = chr(code)
         values = {}
+        attrs = "name category east_asian_width"
         for attr in attrs.split():
             try:
                 values[attr] = getattr(unicodedata, attr)(char)
             except ValueError:
                 values[attr] = "undefined"
-        CHAR_BASE[i] = Character(i, char, values["name"], values["category"], values["east_asian_width"])
+        CHAR_BASE[code] = Character(char, code, values["name"], values["category"], values["east_asian_width"])
 
 
 def lookup(name_part, chars_only=False):
