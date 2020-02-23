dac0248689b51d410feac1391962dad119d4be8c
Joao S O Bueno <gwidion@gmail.com>
2020-02-23 02:36:31 -0300

    [WIP] rendering tests boilerplate factored out to decorator

--- tests/test_terminal_rendering.py
+++ tests/test_terminal_rendering.py
@@ -1,5 +1,6 @@
 import io
 import re
+from functools import wraps
 from unittest import mock
 
 import pytest
@@ -21,40 +22,49 @@ fast_and_slow_render_mark = ("set_render_method", [
     (lambda: setattr(TM.context, "fast_render", True)),
 ])
 
+
+def rendering_test(func):
+    #@wraps(func)
+    def rendering_test(set_render_method, DISPLAY, DELAY):
+        set_render_method()
+        stdout = io.StringIO()
+
+        fn = func()
+        with mock.patch("sys.stdout", stdout):
+            next(fn)
+
+        if DISPLAY:
+            print(stdout.getvalue())
+            TM.pause(DELAY)
+        try:
+            fn.send(stdout.getvalue())
+        except StopIteration:
+            pass
+    rendering_test.__name__ = func.__name__
+
+    return rendering_test
+
 @pytest.mark.parametrize(*fast_and_slow_render_mark)
-def test_render_spaces_default_color_work(set_render_method, DISPLAY, DELAY):
-    set_render_method()
-    stdout = io.StringIO()
+@rendering_test
+def test_render_spaces_default_color_work():
+    sc = TM.Screen(size=(3,3))
+    sc.update()
 
-    with mock.patch("sys.stdout", stdout):
-
-        sc = TM.Screen(size=(3,3))
-        sc.update()
-
-    data = strip_ansi_seqs(stdout.getvalue())
-    if DISPLAY:
-        print(stdout.getvalue())
-        TM.pause(DELAY)
-
+    data = strip_ansi_seqs((yield None))
     assert data == TM.values.EMPTY * 9
 
 
 @pytest.mark.parametrize(*fast_and_slow_render_mark)
-def test_render_blocks_default_color_work(set_render_method, DISPLAY, DELAY):
-    set_render_method()
-    stdout = io.StringIO()
+@rendering_test
+def test_render_blocks_default_color_work():
 
     FB = TM.values.FULL_BLOCK
 
-    with mock.patch("sys.stdout", stdout):
+    sc = TM.Screen(size=(3,3))
+    sc.data[0,0] = FB
+    sc.data[2,2] = FB
+    sc.update()
 
-        sc = TM.Screen(size=(3,3))
-        sc.data[0,0] = FB
-        sc.data[2,2] = FB
-        sc.update()
+    data = strip_ansi_seqs((yield None))
 
-    data = strip_ansi_seqs(stdout.getvalue())
-    if DISPLAY:
-        print(stdout.getvalue())
-        TM.pause(DELAY)
     assert data == FB + TM.values.EMPTY * 7 + FB
