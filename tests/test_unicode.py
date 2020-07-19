cafb4db61691e1c4502bd750fb6501be21f84ee6
Joao S O Bueno <gwidion@gmail.com>
2020-07-19 01:45:56 -0300

    Factored iterator out of 'list_grapheme'

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -11,3 +11,17 @@ def test_split_grapheme_works():
     assert a[0] == grapheme
     assert a[1] == "b"
     assert a[2] == grapheme
+
+
+def test_graphemeiter_works():
+    tilde = chr(0x303)
+    cedilla = chr(0x327)
+    grapheme = "a" + tilde + cedilla
+    msg = grapheme + "b" + grapheme
+
+    a = GraphemeIter(msg)
+    assert len(a) == 3
+    b = iter(a)
+    assert next(b) == grapheme
+    assert next(b) == "b"
+    assert next(b) == grapheme
