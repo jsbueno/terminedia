ca47a6b5834b8dbda6a91a8b902e64f028b5e33f
Joao S O Bueno <gwidion@gmail.com>
2021-04-23 01:00:09 -0300

    Async input, with line editing + edge cases

--- terminedia/asynchronous.py
+++ terminedia/asynchronous.py
@@ -48,46 +48,68 @@ async def terminedia_main(screen=None, context=None):
         await asyncio.sleep(1 / context.fps)
 
 
+def _refresh_line(text, pos, max_pos, backspace=0):
+    delete_back = '\b' * backspace
+    text = ''.join(text)
+    clear = " " * ((max_pos - pos) - len(text) + backspace)
+    move_back = (len(text) + len(clear)) * "\b"
+    terminedia.print(delete_back, text, clear, move_back, sep="", end="", flush=True)
+
+
 async def ainput(prompt="", maxwidth=None, insert=True):
     result = []
     if prompt:
         terminedia.print(prompt, end="", flush=True)
     with terminedia.keyboard:
         keyboard_events = Subscription(KeyPress)
-        pos = 0
+        max_pos = pos = 0
         async for event in keyboard_events:
-            key = event.key
+            print_code = key = event.key
             if key == KeyCodes.ENTER:
                 keyboard_events.kill()
             allow_print = True
             if key:
                 if key == KeyCodes.RIGHT:
-                    if maxwidth is not None and pos < len(result):
+                    if pos < len(result) and (maxwidth is None or pos < maxwidth - 1):
                         pos += 1
                     else:
                         allow_print = False
-                if key == KeyCodes.LEFT:
+                elif key == KeyCodes.LEFT:
                     if pos > 0:
                         pos -= 1
                     else:
                         allow_print = False
-                #if key == KeyCodes.DELETE:
-                    #if len(result) > pos:
-                        #del result[pos]
-                if key in {KeyCodes.UP, KeyCodes.DOWN}:
+                elif key == KeyCodes.DELETE:
+                    if len(result) > pos:
+                        del result[pos]
+                        _refresh_line(result[pos:], pos, max_pos)
+                    allow_print = False
+                elif key == KeyCodes.BACK and pos > 0:
+                    pos -= 1
+                    del result[pos]
+                    _refresh_line(result[pos:], pos, max_pos + 2, backspace=1)
+                    allow_print = False
+                elif key in KeyCodes.codes:
                     allow_print = False
 
-                if allow_print:
-                    terminedia.print(key, end="", flush=True)
+            allow_new_char =  maxwidth is None or len(result) < maxwidth
             if key not in KeyCodes.codes:
-                if insert and (maxwidth is None or len(result) < maxwidth):
+                if allow_new_char and pos == len(result):
+                    result.append(key)
+                elif insert and allow_new_char: # (and pos <= len(result):)
                     result.insert(pos, key)
-                    pos += 1
-                elif not insert:
-                    if pos == len(result) and (maxwidth is None or len(result) < maxwidth):
-                        result.append(key)
-                    elif pos < len(result):
+                    _refresh_line(result[pos:], pos + 1, max_pos)
+                elif not insert and pos < len(result):
                         result[pos] = key
+                        pos += 1
+                else:
+                    allow_print = False
+                if allow_new_char:
+                    pos += 1
+
+            if allow_print:
+                terminedia.print(print_code, end="", flush=True)
+            max_pos = max(max_pos, pos)
 
 
     return ''.join(result)
