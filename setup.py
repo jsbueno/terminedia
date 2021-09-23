803454c56360344569d2610037129130062d2e98
Joao S O Bueno <gwidion@gmail.com>
2021-09-22 22:23:28 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="terminedia",
     packages=find_packages(),
-    version="0.4.1",
+    version="0.4.2",
     license="LGPLv3+",
     author="João S. O. Bueno",
     author_email="gwidion@gmail.com",
