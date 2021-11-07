aa0c91e637a535d3d864ee97db64ed8c06334f17
Joao S O Bueno <gwidion@gmail.com>
2021-11-06 22:56:32 -0300

    Constrain plot to screen rectangle. Fix issue #27

--- terminedia/examples/plot.py
+++ terminedia/examples/plot.py
@@ -2,7 +2,7 @@ import time
 
 import click
 
-from terminedia import Screen, keyboard, inkey, pause
+from terminedia import Screen, pause, Rect
 
 
 def arange(start, stop=None, step=1):
@@ -13,17 +13,18 @@ def arange(start, stop=None, step=1):
         start += step
 
 
-def test_plot(scr, func, domain=(-2, 2)):
+def plot(sc, func, domain=(-2, 2)):
 
-    f = eval(f"lambda x: -({func})")
+    if isinstance(func, str):
+        f = eval(f"lambda x: -({func})")
+    else:
+        f = func
 
-    w, h = scr.high.get_size()
-    scr.context.color = 1, 1, 1
+    w, h = sc.high.size
+    sc.context.color = 1, 1, 1
 
-    scr.high.draw.line((0, h // 2), (w - 1, h // 2))
-    scr.high.draw.line((w // 2, 0), (w // 2, h - 1))
-
-    scale_x = 1 / (w * 1 / (domain[1] - domain[0]))
+    sc.high.draw.line((0, h // 2), (w - 1, h // 2))
+    sc.high.draw.line((w // 2, 0), (w // 2, h - 1))
 
     scale_x = 1 / w * (domain[1] - domain[0])
     domain_center = (domain[1] + domain[0]) / 2
@@ -32,29 +33,34 @@ def test_plot(scr, func, domain=(-2, 2)):
 
     image = min(data_points), max(data_points)
     scale_y = h * 1 / (image[1] - image[0])
+    physical_min_y = -(h / 2) / scale_y
 
-    scr.context.color = 1, 0.5, 0
+    sc.context.color = 1, 0.5, 0
+
+    rect = Rect((w,h))
     for x, y in zip(range(w), data_points):
-        scr.high.draw.set((x, int(y * scale_y)))
+        screen_y = int(y * scale_y)
+        if (x, screen_y) in rect:
+            sc.high.draw.set((x, screen_y))
 
     x_ticks = 10
     y_ticks = 6
 
-    scr.context.color = 0, 1, 0
+    sc.context.color = 0, 1, 0
     for x, x_tick in zip(
         range(0, w, w // x_ticks),
         arange(domain[0], domain[1], (domain[1] - domain[0]) / x_ticks),
     ):
-        scr.print_at((x // 2, h // 4 + 1), f"{x_tick:0.02f}")
+        sc.print_at((x // 2, h // 4 + 1), f"{x_tick:0.02f}")
 
     for y, y_tick in zip(
         range(0, h, h // y_ticks),
         arange(image[1], image[0], -(image[1] - image[0]) / y_ticks),
     ):
 
-        scr.print_at((w // 4 + 1, y // 2), f"{y_tick:0.02f}")
+        sc.print_at((w // 4 + 1, y // 2), f"{y_tick:0.02f}")
 
-    scr.print_at(
+    sc.print_at(
         (w // 2 - 33, 0), f"f(x) = {func.replace('**3', '³').replace('**2', '²')}"
     )
 
@@ -68,8 +74,8 @@ def test_plot(scr, func, domain=(-2, 2)):
 def main(func=None, domain=(-2, 2)):
     if func is None:
         func = "-2 * x**3 - 3 * x**2 + x - 1"
-    with Screen() as scr:
-        test_plot(scr, func, domain)
+    with Screen() as sc:
+        plot(sc, func, domain)
         pause()
 
 
